b5bebe46bbead1f69532de6bba32e02cc69bb5ac
Your Name <amkomarudin7@gmail.com>
2019-12-04 15:18:44 +0700

    perubahan dari CI menjadi Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,218 @@
+@extends('layout.main')
+@section('title','Daftar Mahasiswa')
+@section('content')
+
+
+<section class="slide1">
+    <div class="wrap-slick1">
+        <div class="slick1">
+            <div class="item-slick1 item1-slick1" style="background-image: url(user/images/TUF-Cat-Ban-01.jpg);">
+                <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
+                    <span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="fadeInDown">
+                        Women Collection 2018
+                    </span>
+
+                    <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="fadeInUp">
+                        New arrivals
+                    </h2>
+
+                    <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="zoomIn">
+                        <!-- Button -->
+                        <a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
+                            Shop Now
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item-slick1 item2-slick1" style="background-image: url(user/images/banner.png);">
+                <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
+                    <span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="rollIn">
+                        Women Collection 2018
+                    </span>
+
+                    <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="lightSpeedIn">
+                        New arrivals
+                    </h2>
+
+                    <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="slideInUp">
+                        <!-- Button -->
+                        <a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
+                            Shop Now
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item-slick1 item3-slick1" style="background-image: url(user/images/banner.png);">
+                <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
+                    <span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="rotateInDownLeft">
+                        Women Collection 2018
+                    </span>
+
+                    <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="rotateInUpRight">
+                        New arrivals
+                    </h2>
+
+                    <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="rotateIn">
+                        <!-- Button -->
+                        <a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
+                            Shop Now
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<section class="instagram p-t-20">
+    <div class="sec-title p-b-52 p-l-15 p-r-15">
+        <h3 class="m-text5 t-center">
+            @ follow us on Instagram
+        </h3>
+    </div>
+
+    <div class="flex-w">
+        <!-- Block4 -->
+        <div class="block4 wrap-pic-w">
+            <img src="user/images/ig1.webp" alt="IMG-INSTAGRAM">
+
+            <a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
+                <span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
+                    <i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
+                    <span class="p-t-2">39</span>
+                </span>
+
+                <div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
+                    <p class="s-text10 m-b-15 h-size1 of-hidden">
+                        Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla
+                        in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget
+                        metus elit.
+                    </p>
+
+                    <span class="s-text9">
+                        Photo by @nancyward
+                    </span>
+                </div>
+            </a>
+        </div>
+
+        <!-- Block4 -->
+        <div class="block4 wrap-pic-w">
+            <img src="user/images/ig2.jpg" alt="IMG-INSTAGRAM">
+
+            <a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
+                <span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
+                    <i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
+                    <span class="p-t-2">39</span>
+                </span>
+
+                <div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
+                    <p class="s-text10 m-b-15 h-size1 of-hidden">
+                        Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla
+                        in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget
+                        metus elit.
+                    </p>
+
+                    <span class="s-text9">
+                        Photo by @nancyward
+                    </span>
+                </div>
+            </a>
+        </div>
+
+        <!-- Block4 -->
+        <div class="block4 wrap-pic-w">
+            <img src="user/images/ig3.jpg" alt="IMG-INSTAGRAM">
+
+            <a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
+                <span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
+                    <i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
+                    <span class="p-t-2">39</span>
+                </span>
+
+                <div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
+                    <p class="s-text10 m-b-15 h-size1 of-hidden">
+                        Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla
+                        in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget
+                        metus elit.
+                    </p>
+
+                    <span class="s-text9">
+                        Photo by @nancyward
+                    </span>
+                </div>
+            </a>
+        </div>
+
+        <!-- Block4 -->
+        <div class="block4 wrap-pic-w">
+            <img src="user/images/ig4.jpg" alt="IMG-INSTAGRAM">
+
+            <a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
+                <span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
+                    <i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
+                    <span class="p-t-2">39</span>
+                </span>
+
+                <div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
+                    <p class="s-text10 m-b-15 h-size1 of-hidden">
+                        Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla
+                        in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget
+                        metus elit.
+                    </p>
+
+                    <span class="s-text9">
+                        Photo by @nancyward
+                    </span>
+                </div>
+            </a>
+        </div>
+
+        <!-- Block4 -->
+        <div class="block4 wrap-pic-w">
+            <img src="user/images/ig5.webp" alt="IMG-INSTAGRAM">
+
+            <a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
+                <span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
+                    <i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
+                    <span class="p-t-2">39</span>
+                </span>
+
+                <div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
+                    <p class="s-text10 m-b-15 h-size1 of-hidden">
+                        Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla
+                        in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget
+                        metus elit.
+                    </p>
+
+                    <span class="s-text9">
+                        Photo by @nancyward
+                    </span>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<!-- Back to top -->
+<div class="btn-back-to-top bg0-hov" id="myBtn">
+    <span class="symbol-btn-back-to-top">
+        <i class="fa fa-angle-double-up" aria-hidden="true"></i>
+    </span>
+</div>
+
+<!-- Container Selection1 -->
+<div id="dropDownSelect1"></div>
+
+
+
+
+@endsection
